a91eb7bf1b96d98d2a9064891e20a58172c9e840
Rahil Ali <rahil.eliyev@gmail.com>
2021-04-29 17:47:35 +0400

    port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 var cors = require("cors");
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
   // Request methods you wish to allow
   res.setHeader(
