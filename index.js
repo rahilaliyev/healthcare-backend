da84ff668be08277506843766b4d346b63bbf985
Rahil Ali <rahil.eliyev@gmail.com>
2021-04-29 17:53:56 +0400

    port

--- index.js
+++ index.js
@@ -6,29 +6,6 @@ require("firebase/firestore");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 var cors = require("cors");
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
 app.use(cors());
 var firebaseConfig = {
   apiKey: "AIzaSyDFzqyobQKGcUJgMpAbUdEaOvGYcI_3TiY",
@@ -47,6 +24,7 @@ app.get("/getData", (req, res) => {
     .doc("XLxHb8O2A3EYa2YAt5Jq")
     .get()
     .then((result) => {
+      res.header("Access-Control-Allow-Origin", "*");
       res.send(result.data());
     });
 });
