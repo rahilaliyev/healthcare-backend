278c57a618da4658faf43b50a978606476bd2fdc
Rahil Ali <rahil.eliyev@gmail.com>
2021-04-29 17:32:42 +0400

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const firebase = require("firebase/app");
-const port = 3000;
+const port = process.env.PORT || 3000;
 require("firebase/firestore");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
