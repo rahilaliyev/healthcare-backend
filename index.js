d941b2ed4917886e0d5266da94e951148733b1c8
Rahil Ali <rahil.eliyev@gmail.com>
2021-05-05 17:48:56 +0400

    stackoverflow

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-//import libraries
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
-import * as express from "express";
-import * as bodyParser from "body-parser";
 const express = require("express");
 const app = express();
 const firebase = require("firebase/app");
@@ -34,39 +29,13 @@ app.get("/getData", (req, res) => {
     });
 });
 
-//initialize firebase inorder to access its services
-admin.initializeApp(functions.config().firebase);
-
-//initialize express server
-const app = express();
-const main = express();
-
-//add the path to receive request and set json as bodyParser to process the body
-main.use("/api/v1", app);
-main.use(bodyParser.json());
-main.use(bodyParser.urlencoded({ extended: false }));
-
-//initialize the database and the collection
-const db = admin.firestore();
-const userCollection = "users";
-
-//define google cloud function name
-export const webApi = functions.https.onRequest(main);
-
-// Create new user
-app.post("/users", async (req, res) => {
-  try {
-  
-
-    const newDoc = await db.collection(userCollection).add(req.body );
-    res.status(201).send(`Created a new user: ${newDoc.id}`);
-  } catch (error) {
-    res
-      .status(400)
-      .send(
-        `User should cointain firstName, lastName, email, areaNumber, department, id and contactNumber!!!`
-      );
-  }
+app.post("/users", (req, res) => {
+  const data = req.body;
+  console.log(data);
+  res.send({
+    success: "200 response",
+    res: "You are now just talked with server",
+  });
 });
 
 app.listen(port, () => {
