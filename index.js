9f299a00619c85d4c6e17db30c89006e12eea031
Rahil Ali <rahil.eliyev@gmail.com>
2021-05-06 16:46:20 +0400

    data

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post("/users", (req, res) => {
   });
   db.collection("data")
     .doc("XLxHb8O2A3EYa2YAt5Jq")
-    .push({ List12: ["salam", "email"] });
+    .add({ List12: ["salam", "email"] });
 });
 
 app.listen(port, () => {
